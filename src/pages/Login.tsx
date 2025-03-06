1214e7e8028e9a303bdc0ae799d011644a2e2e52
Joanbensulan <joanbensulan2@gmail.com>
2025-03-06 17:31:44 +0800

    MOdified src/pages/login.tsx and hom-tabs/feed.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,28 +1,77 @@
 import { 
+  IonAvatar,
     IonButton,
     IonButtons,
       IonContent, 
       IonHeader, 
+      IonIcon, 
+      IonInput, 
+      IonInputPasswordToggle, 
+      IonItem, 
       IonMenuButton, 
       IonPage, 
       IonTitle, 
       IonToolbar, 
       useIonRouter
   } from '@ionic/react';
+import { logoFacebook, logoInstagram, logoIonic, logoTiktok } from 'ionicons/icons';
   
   const Login: React.FC = () => {
     const navigation = useIonRouter();
+  
     const doLogin = () => {
         navigation.push('/it35-lab/app','forward','replace');
     }
     return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <IonContent className='ion-padding'>
+
+        <div style={{
+                  display: 'flex',
+                  flexDirection:'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  height: '100%',
+                  width:'100%',
+                  marginTop:'-10rem',
+                  marginBottom:'-18rem',
+                }}>
+             <IonAvatar
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      width: '150px',
+                      height: '150px',
+                      borderRadius: '50%', 
+                      overflow: 'hidden' 
+                    }}
+                  >
+                   
+                    {
+                     <IonIcon 
+                      icon={logoTiktok}
+                      color='black'
+                      style={{ fontSize: '120px', color: '#6c757d' }} 
+                    />
+                    }
+                  </IonAvatar>
+                  <h1 style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>USER LOGIN</h1>
+                    
+          </div>
+          
+            <IonTitle>Login</IonTitle>
+          <IonItem>
+        <IonInput label="Email input" type="email" placeholder="joanbensulan2@gmail.com"></IonInput>
+      </IonItem>
+
+        <IonInput type="password" label="Password" value="HELLOKITTY">
+      <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+    </IonInput>
             <IonButton onClick={() => doLogin()} expand="full">
                 Login
             </IonButton>
