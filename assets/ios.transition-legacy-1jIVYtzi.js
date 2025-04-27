25e6a47a6ad441cf8de9d75cf7539b2f2e6ab3cd
Joanbensulan <joanbensulan2@gmail.com>
2025-04-27 14:01:24 +0800

    Updates

--- assets/ios.transition-legacy-1jIVYtzi.js
+++ assets/ios.transition-legacy-1jIVYtzi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MXiAnmi-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DDDq7iAS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
